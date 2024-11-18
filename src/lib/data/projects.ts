e038301243ad2c51616dcdb3b09d85b29050d2dd
Mosiur Rahman Sweet <sweet.csevu@gmail.com>
2024-11-18 19:48:29 +0600

    image fix

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -4,6 +4,55 @@ import type { Project } from '../types';
 
 export const items: Array<Project> = [
 	{
+		slug: 'expense-manager',
+		color: '#5e95e3',
+		description: 'Wise Owl - Expense Manager is the perfect companion for managing your personal finances in a simple, user-friendly, and secure way. Whether you want to keep track of your income or expenses, Wise Owl makes it easy to manage your financial data efficiently, with a minimalistic and intuitive design.',
+			shortDescription:
+			'Track income and expenses easily with Wise Owl—secure, offline, and insightful.',
+			links: [{ to: 'https://play.google.com/store/apps/details?id=dev.orangeowl.wise_owl', label: 'Playstore' }],
+		logo: Assets.Flutter,
+		name: 'Wise Owl - Expense Manager App',
+		period: {
+			from: new Date(2024, 9, 1), to: new Date(2024, 10, 5),
+		},
+		skills: getSkills('flutter', 'dart'),
+		type: 'Cross Platform App',
+		screenshots: [
+			{
+				label: 'Splashh',
+				src: 'https://i.ibb.co.com/6Xmy23V/Screenshot-1729748406.png'
+			},
+			{
+				label: 'Update Info',
+				src: 'https://i.ibb.co.com/120420H/Screenshot-1729748397.png'
+			},
+			{
+				label: 'Add Transaction - Light Mode',
+				src: 'https://i.ibb.co.com/QcW7THd/Screenshot-1729746759.png'
+			},
+			{
+				label: 'Add Transaction - Dark Mode',
+				src: 'https://i.ibb.co.com/QDW5CFL/Screenshot-1729746794.png'
+			},
+			{
+				label: 'Home Screen - Light Mode',
+				src: 'https://i.ibb.co.com/HT8s0ZY/Screenshot-1729748243.png'
+			},
+			{
+				label: 'Transaction Log',
+				src: 'https://i.ibb.co.com/prWGkc8/Screenshot-1729748327.png'
+			},
+			{
+				label: 'Home Screen - Dark Mode',
+				src: 'https://i.ibb.co.com/HdMNfSZ/Screenshot-1729748330.png'
+			},
+			{
+				label: 'Statistics',
+				src: 'https://i.ibb.co.com/MZSbVQj/Screenshot-1729748364.png'
+			},
+		],
+	},
+	{
 		slug: 'athena-gpt-flutter',
 		color: '#5e95e3',
 		description: 'Leveraged the capabilities of ChatGPT 3.5 with OpenAPI Integration. Implemented riverpod as state management and utilized speech-to-text and chatgpt_sdk package for natural conversations.',
@@ -16,12 +65,18 @@ export const items: Array<Project> = [
 			from: new Date(2024, 4, 1), to: new Date(2024, 4, 15),
 		},
 		skills: getSkills('flutter', 'dart', 'firebase'),
-		type: 'Cross Platform App'
+		type: 'Cross Platform App',
+		screenshots: [
+			{
+				label: 'Feature Graphics',
+				src: 'https://i.ibb.co.com/qWgxwXZ/d-TRt-B3-AIHz-G4-1024-500.png'
+			},
+		],
 	},
 	{
 		slug: 'tong-flutter',
 		color: '#5e95e3',
-		description: 'Tong allows users to virtually connect with others through video, audio, and chat. Built using firebase and jitsi SDK',
+		description: 'Tong is a communication platform that enables users to connect virtually through video calls, audio calls, and instant chat. Built with Firebase for seamless backend integration and the Jitsi SDK for reliable and high-quality video conferencing, the app delivers a smooth and interactive communication experience. Whether for personal or professional use, Tong is designed to keep users connected anytime, anywhere.',
 			shortDescription:
 			'Tong allows users to virtually connect with others through video, audio, and chat. Built using firebase and jitsi SDK',
 			links: [{ to: 'https://github.com/5w33t/flutter_projects/tree/main/tong_app', label: 'GitHub' }],
@@ -31,7 +86,22 @@ export const items: Array<Project> = [
 			from: new Date(2024, 5, 1), to: new Date(2024, 5, 21),
 		},
 		skills: getSkills('flutter', 'dart', 'firebase'),
-		type: 'Cross Platform App'
+		type: 'Cross Platform App',
+		
+		screenshots: [
+			{
+				label: 'Home Screen',
+				src: 'https://i.ibb.co.com/Sn4XD82/68747470733a2f2f692e6962622e636f2f53775a4b3933462f686f6d652e6a7067.jpg'
+			},
+			{
+				label: 'Join Adda',
+				src: 'https://i.ibb.co.com/N9thjT2/68747470733a2f2f692e6962622e636f2f7a7272793237372f6a6f696e2d616464612e6a7067.jpg'
+			},
+			{
+				label: 'Sign Out',
+				src: 'https://i.ibb.co.com/BZcnDT1/68747470733a2f2f692e6962622e636f2f67766d795864742f6c6f672d6f75742e6a7067.jpg'
+			},
+		],
 	},
 
 	{
@@ -43,7 +113,7 @@ export const items: Array<Project> = [
 			'Access accurate Sehri and Iftar timings. Built using the BLoC architecture for efficient state management',
 		links: [{ to: 'https://github.com/5w33t/flutter_projects/tree/main/ramadan_app', label: 'GitHub' }],
 		logo: Assets.Flutter,
-		name: 'Ramdan App',
+		name: 'Ramadan App',
 		period: {
 			from: new Date(2024, 5, 1), to: new Date(2024, 5, 7),
 		},
@@ -51,29 +121,10 @@ export const items: Array<Project> = [
 		type: 'Cross Platform App',
 		screenshots: [
 			{
-				label: 'screen 1',
-				src: 'https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+				label: 'Home Screen',
+				src: 'https://ibb.co.com/RB1b0My'
 			},
-			{
-				label: '2',
-				src: 'https://images.unsplash.com/photo-1516116216624-53e697fedbea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '3',
-				src: 'https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '4',
-				src: 'https://images.unsplash.com/photo-1542903660-eedba2cda473?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '5',
-				src: 'https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '6',
-				src: 'https://images.unsplash.com/photo-1585079542156-2755d9c8a094?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			}
+			
 		]
 	}
 ];
